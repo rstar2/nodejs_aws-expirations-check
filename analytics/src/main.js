b67727356902105a07c12b4ea6df7b99c3181ae8
Rumen Neshev <rumen@KOMERO.NET>
2019-10-17 17:15:03 +0300

    asd

--- analytics/src/main.js
+++ analytics/src/main.js
@@ -8,7 +8,7 @@ new Vue({
 }).$mount("#app");
 
 // eslint-disable-next-line no-console
-console.log(process.env.VUE_APP_API_URL + '    ' + process.env.VUE_APP_GOOGLE_ANALYTICS_VIEW_ID);
+console.log(process.env.VUE_APP_API_URL);
 // fetch(process.env.VUE_APP_API_URL)
 //     .then(res => res.json())
 //     .then(data => data.data)
