d1e764f9f9fbcc32d8c8b8aac7646d3deb65c4d7
Rumen Neshev <rstar2@abv.bg>
2019-10-19 23:32:04 +0300

    asdasd

--- test/express/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-app.use(cors());
-
-app.use('bands', require('./routes/bands'));
-
-module.exports = app;
--- test/express/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const server = require('http').createServer(app);
-
-// Config
-server.listen(5000, () => {
-    // eslint-disable-next-line no-console
-    console.log('Server running at localhost:5000');
-});
